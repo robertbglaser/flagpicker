4142b4a952ad2acef1d2b399f547e7d69ad7f9c9
Robert Glaser <robertbglaser@gmail.com>
2019-01-09 12:43:32 -0800

    new approach to filter

--- src/Step2.js
+++ src/Step2.js
@@ -2,26 +2,63 @@ import React, { Component } from 'react';
 import Continents from "./data/continents.json";
 
 export default class Step2 extends Component {
+  constructor(props){
+    super(props)
+    this.state ={
+      selectedRegion:"Asia",
+      continentList: [],
+
+    }
+
+
+
+  };
+
+  componentDidMount() {
+    const regionArr =[];
+    Continents.map((foo) => {
+       return ( 
+         regionArr.push(foo.continent)
+       )
+       
+    })
+    this.setState({continentList:regionArr});
+
+   // console.log(regionArr)
+  }
+  
   render() {
 
-    const selectedRegion = (this.props.continent);  
+    //const selectedRegion = (this.props.continent); 
+    //const  = this.state.region;
+    console.log("before the loop"); 
 
-        let continentFiltered = Continents.filter((region) => {
-          return region.continent === {selectedRegion}
+      for (var x in this.state.continentList){
+        console.log("in the loop" + this.state.continentList[x])
+         if (this.state.continentList[x] === this.state.selectedRegion){
+              console.log("match")
+         } 
+      }
+  
+      /*  let continentFiltered = Continents.filter((Asia) => {
+          return region.continent === 'Asia'
         });
         
-        //console.log("the values are = ",  ages1)
+        console.log("In Step 2, the values are = ",  continentFiltered)
         
         const countryList = continentFiltered.map((info, i) => {
               return (
-               //  console.log(info.continent),
+                console.log(info.continent),
                  info.countries.map((country, idx) =>
-                   console.log(country.name, country.flag)
+                 console.log("in the countries map " + country.name)
+                 
                 )
-            )});
+            )});  */
       
     return (
       <div>
+
+       
         
       </div>
     )
