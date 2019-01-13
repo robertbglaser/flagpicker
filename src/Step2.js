42a558e8cfb65e5394185506d61f28b98738916d
Robert Glaser <robertbglaser@gmail.com>
2019-01-13 14:54:07 -0800

    saving changes

--- src/Step2.js
+++ src/Step2.js
@@ -7,49 +7,65 @@ export default class Step2 extends Component {
     this.state ={
       selectedRegion:"Asia",
       continentList: [],
-      listOCountries:[]
+      listOCountries:[],
+      countrySelected: "",
 
     }
-
-
-
   };
 
   componentDidMount() {
-    const regionArr =[];
+ //   const regionArr =[];
     const selectedRegion = (this.state.selectedRegion); 
     var countryArr = [];
-    Continents.map((foo) => {
-       return ( 
-         regionArr.push(foo.continent)
-       )
-       
-    })
-    this.setState({continentList:regionArr});
+    
 
     Continents.forEach(function(region,index){
       if ( selectedRegion === region.continent){
            countryArr= region.countries
-      }
-       
+      }  
    });
-
-   //console.log(countryArr)
-
-   this.setState({listOCountries: countryArr});
+    this.setState({listOCountries: countryArr});
   }
-  
+  handleCheckItem = (e)=>{
+
+    console.log("in handleCheckItem = ", e.target.value)
+    
+     const  countrySelected = e.target.value;
+      this.setState({countrySelected: countrySelected});
+
+       console.log("%%" + this.state.countrySelected);
+  }
   render() {
 
-    console.log("in step 2 render" , this.state.listOCountries)    
+    console.log("in step 2 render" , this.state.listOCountries)
+
+  
+    
+    const countries = this.state.listOCountries.map(((foo) => {
+      return ( 
+        <div key={foo.name} >
+            <li className="step2List"> 
+            <input type="checkbox" onChange={this.handleCheckItem} value={foo.name}/>{foo.name}
+            </li>
+           
+        </div>
+      )
+      
+    }));
+
+   
+
+    
+    
+
     return (
       <div>
 
         <ul>
-
+        {countries}
 
         </ul>
-
+       
        
         
       </div>
